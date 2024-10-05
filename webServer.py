bba00ecde3400ec3e47213a71eca00e9d537b9cf
gfs7337 <gfs7337@nyu.edu>
2024-10-05 11:54:32 -0400

    Attempt 26
    Attempt 26

--- webServer.py
+++ webServer.py
@@ -46,8 +46,8 @@ def webServer(port=13331):
       #Fill in end
                
       for i in f:#for line in file
-        connectionSocket.sendall(response)
-        connectionSocket.sendall(file_content)
+        connectionSocket.send(response)
+        connectionSocket.send(file_content)
       #Fill in start - append your html file contents #Fill in end
 
       #Send the content of the requested file to the client (don't forget the headers you created)!
